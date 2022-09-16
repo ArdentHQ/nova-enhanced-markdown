01d1241b94f3b84f57d211118c0c3cc68bbb8867
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2022-09-16 12:56:51 +0100

    chore: have editorconfig match other projects (#1)

--- tests/EnhancedMarkdownTest.php
+++ tests/EnhancedMarkdownTest.php
@@ -9,37 +9,37 @@ use Laravel\Nova\Trix\DetachAttachment;
 use Laravel\Nova\Trix\DiscardPendingAttachments;
 
 it('creates an instance', function () {
-	$field = new EnhancedMarkdown('content');
+    $field = new EnhancedMarkdown('content');
 
-	expect($field->component)->toBe('enhanced-markdown');
+    expect($field->component)->toBe('enhanced-markdown');
 
-	expect($field->jsonSerialize())->toMatchArray([
-		'uniqueKey' => 'content-default-enhanced-markdown',
-		'attribute' => 'content',
-		'component' => 'enhanced-markdown',
-	]);
+    expect($field->jsonSerialize())->toMatchArray([
+        'uniqueKey' => 'content-default-enhanced-markdown',
+        'attribute' => 'content',
+        'component' => 'enhanced-markdown',
+    ]);
 });
 
 it('accepts files', function () {
-	$field = new EnhancedMarkdown('content');
+    $field = new EnhancedMarkdown('content');
 
-	$field->withFiles('disk-name', 'custom-path');
+    $field->withFiles('disk-name', 'custom-path');
 
-	expect($field->withFiles)->toBeTrue();
-	expect($field->prunable)->toBeTrue();
+    expect($field->withFiles)->toBeTrue();
+    expect($field->prunable)->toBeTrue();
 
-	expect($field->attachCallback)->toBeInstanceOf(StorePendingAttachment::class);
-	expect($field->detachCallback)->toBeInstanceOf(DetachAttachment::class);
-	expect($field->deleteCallback)->toBeInstanceOf(DeleteAttachments::class);
-	expect($field->discardCallback)->toBeInstanceOf(DiscardPendingAttachments::class);
+    expect($field->attachCallback)->toBeInstanceOf(StorePendingAttachment::class);
+    expect($field->detachCallback)->toBeInstanceOf(DetachAttachment::class);
+    expect($field->deleteCallback)->toBeInstanceOf(DeleteAttachments::class);
+    expect($field->discardCallback)->toBeInstanceOf(DiscardPendingAttachments::class);
 });
 
 it('accepts a preset', function () {
-	$field = new EnhancedMarkdown('content');
+    $field = new EnhancedMarkdown('content');
 
-	expect($field->preset)->toBe('default');
+    expect($field->preset)->toBe('default');
 
-	$field->preset('commonmark');
+    $field->preset('commonmark');
 
-	expect($field->preset)->toBe('commonmark');
+    expect($field->preset)->toBe('commonmark');
 });
