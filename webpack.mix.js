017a149360383b0cfa5d5101fd69d50a93ef161d
ItsANameToo <itsanametoo@protonmail.com>
2022-09-16 13:53:19 +0200

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,12 +3,11 @@ let mix = require("laravel-mix");
 
 require("./nova.mix");
 
-mix
-	.alias({
-		"@": path.join(__dirname, "/vendor/laravel/nova/resources/js/"),
-	})
-	.webpackConfig({ output: { uniqueName: "laravel/nova" } })
-	.setPublicPath("dist")
-	.js("resources/js/field.js", "js")
-	.vue({ version: 3 })
-	.nova("ardenthq/nova-enhanced-markdown");
+mix.alias({
+    "@": path.join(__dirname, "/vendor/laravel/nova/resources/js/"),
+})
+    .webpackConfig({ output: { uniqueName: "laravel/nova" } })
+    .setPublicPath("dist")
+    .js("resources/js/field.js", "js")
+    .vue({ version: 3 })
+    .nova("ardenthq/nova-enhanced-markdown");
